d2cdea72a80037f43198de36d3ae492b49559662
aswinkarthik93 <aswinkarthik93@gmail.com>
2017-10-08 14:42:06 +0530

    Checks if args passed are valid stacks before generation

---
+++ index.js
@@ -0,0 +1,54 @@
+const request = require('request'),
+      axios = require('axios'),
+      fs = require('fs')
+
+
+const baseUrl = 'https://www.gitignore.io/api/'
+const listUrl = `${baseUrl}list`
+
+let cliArgs = process.argv
+
+cliArgs.shift()
+cliArgs.shift()
+
+cliArgsToCheck = cliArgs.map(val => val.trim().toLowerCase())
+
+if(cliArgsToCheck == null || cliArgsToCheck.length == 0) {
+  console.log('Please pass atleast 1 argument')
+  process.exit(1)
+}
+
+axios
+  .get(listUrl)
+  .then(({data}) => {
+    const supportedStacks = data
+      .replace(/\n/g,',')
+      .split(',')
+    let notSupportedStacks = []
+
+    cliArgsToCheck.forEach(each => {
+      if(!supportedStacks.includes(each)){
+        notSupportedStacks.push(each)
+      }
+    });
+    return notSupportedStacks
+  })
+  .then(notSupportedItems => {
+    if( notSupportedItems != null && notSupportedItems.length > 0) {
+      console.log('The following keywords are not supported: \n')
+      notSupportedItems.forEach(each => console.log(each))
+      console.log('\n\nExiting.\n')
+      process.exit(1)
+    }
+  })
+  .then(() => {
+    const apiStacks = encodeURIComponent(cliArgsToCheck.join())
+    return axios.get(baseUrl + apiStacks)
+                .then(res => fs.writeFile('gitignore.io', res.data, err => console.log(err)))
+                .catch(err => console.log(err))
+  })
+  .then(() => {
+    console.log('Generated ./gitignore.io file')
+  })
+  .catch(err => console.log(err))
+
