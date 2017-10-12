437b2ad5e2e67575898a34af15107f2475337c80
aswinkarthik93 <aswinkarthik93@gmail.com>
2017-10-12 22:35:46 +0530

    Fuzzy search across all options

--- src/index.js
+++ src/index.js
@@ -3,38 +3,67 @@
 const chalk = require('chalk'),
       log = console.log,
       axios = require('axios'),
-      fs = require('fs');
+      fs = require('fs'),
+      Fuse = require('fuse.js');
 
 const baseUrl = 'https://www.gitignore.io/api/';
 const listUrl = `${baseUrl}list`;
 
 const defaultTargetFile = 'gitignore.cli';
 
-const getSupportedListAndDo = () => {
+const logger = {
+  info: msg => console.log(msg),
+  success: msg => console.log(chalk.green(msg)),
+  error: msg => console.log(chalk.red(msg))
+};
+
+const fuseOptions = {
+  shouldSort: true,
+  threshold: 0.3,
+  location: 0,
+  distance: 10,
+  maxPatternLength: 32,
+  minMatchCharLength: 1
+};
+
+const getList = () => {
   return axios
   .get(listUrl)
   .then(({data}) => {
     const supportedStacks = data.replace(/\n/g,',').split(',');
     return supportedStacks;
   });
-}
+};
 
 const listOptions = () => {
-  getSupportedListAndDo()
+  getList()
   .then(supportedStacks => {
     log(chalk.green('Available options \n'));
     log(supportedStacks.join(','));
     log('\n');
   });
-}
+};
+
+const searchOptions = (item) => {
+  logger.info(`Searching for ${chalk.green(item)}\n`);
+  getList()
+    .then((supportedStacks) => {
+      const engine = new Fuse(supportedStacks, fuseOptions);
+      const searchResults = engine.search(item);
+      if(searchResults.length < 1) {
+        logger.error('No results found');
+      }
+      searchResults.forEach(res => logger.info(supportedStacks[res]));
+    });
+};
 
 const setOutputFilename = (filename) => {
   selectedTargetFile = filename;
   log(selectedTargetFile);
-}
+};
 
 const checkIfValid = (options) => {
-  return getSupportedListAndDo()
+  return getList()
   .then(supportedStacks => {
     let notSupportedStacks = [];
     options.forEach(e => {
@@ -47,8 +76,7 @@ const checkIfValid = (options) => {
       log(notSupportedStacks.join() + '\n');
     }
   })
-}
-
+};
 
 const generate = (options, outputfile) => {
   if(program.args.length < 1) {
@@ -66,13 +94,14 @@ const generate = (options, outputfile) => {
       return targetFile;
     })
     .then((targetFile) => log(chalk.green(`Done`)))
-}
+};
 
 const program = require('commander');
 program
   .version(chalk.bold('1.2.0'))
   .option('-l, --list', 'List all available stacks', listOptions)
   .option('-o, --outputfile [value]', `Default is ${chalk.bold(defaultTargetFile)}`)
+  .option('-s, --search <item>', 'Fuzzy search across various options', searchOptions)
   .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
